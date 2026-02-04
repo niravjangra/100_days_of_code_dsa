0c464c47ba3b5f2315bc420564f8b071af2f67e3
niravjangra <NIRAV.21841@stu.upes.ac.in>
2026-02-04 22:52:44 +0530

    Add files via upload

---
+++ dsa_day1_ques1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, x, pos, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter element to insert: ");
+    scanf("%d", &x);
+    printf("Enter position (1-based): ");
+    scanf("%d", &pos);
+    if(pos < 1 || pos > n + 1) {
+        printf("Invalid position!\n");
+        return 0;
+    }
+    for(i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[pos - 1] = x;
+    n++;
+    printf("Array after insertion:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
